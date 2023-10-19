3f4709b901d130b3de45f337eb88b55d8cffb7bd
BenGicheru <bensgich@gmail.com>
2023-10-19 17:37:02 +0300

    Rearrange footer items

--- lib/src/sidebarx_base.dart
+++ lib/src/sidebarx_base.dart
@@ -141,32 +141,30 @@ class _SidebarXState extends State<SidebarX>
                 ),
               ),
               widget.footerDivider ?? const SizedBox(),
+              if (widget.footerItems.isNotEmpty)
+                ListView.separated(
+                  reverse: true,
+                  itemCount: widget.footerItems.length,
+                  separatorBuilder: widget.separatorBuilder ??
+                          (_, __) => const SizedBox(height: 8),
+                  itemBuilder: (context, index) {
+                    final item = widget.footerItems.reversed.toList()[index];
+                    return SidebarXCell(
+                      item: item,
+                      theme: t,
+                      animationController: _animationController!,
+                      extended: widget.controller.extended,
+                      selected: widget.controller.selectedIndex ==
+                          widget.items.length +
+                              widget.footerItems.length -
+                              index -
+                              1,
+                      onTap: () => _onFooterItemSelected(item, index),
+                    );
+                  },
+                ),
               widget.footerBuilder?.call(context, widget.controller.extended) ??
                   const SizedBox(),
-              if (widget.footerItems.isNotEmpty)
-                Expanded(
-                  child: ListView.separated(
-                    reverse: true,
-                    itemCount: widget.footerItems.length,
-                    separatorBuilder: widget.separatorBuilder ??
-                        (_, __) => const SizedBox(height: 8),
-                    itemBuilder: (context, index) {
-                      final item = widget.footerItems.reversed.toList()[index];
-                      return SidebarXCell(
-                        item: item,
-                        theme: t,
-                        animationController: _animationController!,
-                        extended: widget.controller.extended,
-                        selected: widget.controller.selectedIndex ==
-                            widget.items.length +
-                                widget.footerItems.length -
-                                index -
-                                1,
-                        onTap: () => _onFooterItemSelected(item, index),
-                      );
-                    },
-                  ),
-                ),
               if (widget.showToggleButton)
                 _buildToggleButton(t, widget.collapseIcon, widget.extendIcon),
             ],
